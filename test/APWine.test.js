f15a53771b6b7d8d178d5bea7f8d999a096bfd14
Ulysse <ulysse@EG.oberholz.zeh.local>
2020-10-31 17:23:22 +0100

    finish initial APWine tests

--- test/APWine.test.js
+++ test/APWine.test.js
@@ -18,22 +18,11 @@ describe("APWine", function () {
 
     beforeEach(async function () {
         this.controller = await APWineController.new()
-        this.tokenFactory = await FutureYieldTokenFactory.new()
-
-        // Deploy Compound
-        this.compToken = await ERC20.new("COMP", "COMP")
-        this.cdaiToken = await ERC20.new("CDAI", "CDAI")
-        this.compoundWeekly = await APWineCompound.new(this.controller.address, this.tokenFactory.address, this.compToken.address, "Compound Weekly", 7)
-        //TODO: refactor APWineCompound, APWineAave initializers to take platforms addresses instead of hardcoded values
-        await this.controller.addFuture(this.compoundWeekly.address)
-
-        // Deploy Aave
-        /*this.aaveWeekly = await APWineAave.new()*/
+        this.token = await ERC20.new("", "")
     })
 
-    it("has registered futures correctly", async function () {
-        expect(await this.controller.futuresCount()).to.be.bignumber.equal(new BN(1))
-        expect(await this.controller.future(0)).to.be.equal(this.compoundWeekly.address)
+    it("has no registered futures by default", async function () {
+        expect(await this.controller.futuresCount()).to.be.bignumber.equal(new BN(0))
     })
 
     it("has no proxy set up by default", async function () {
@@ -58,34 +47,34 @@ describe("APWine", function () {
             await this.controller.createProxy({ from: user2 })
             this.proxy1 = await APWineProxy.at(await this.controller.proxiesByUser(user1))
             this.proxy2 = await APWineProxy.at(await this.controller.proxiesByUser(user2))
-            await this.cdaiToken.mint(user1, amount("1000"))
-            await this.cdaiToken.mint(user2, amount("1000"))
+            await this.token.mint(user1, amount("1000"))
+            await this.token.mint(user2, amount("1000"))
         })
 
         it("can send tokens to a proxy", async function () {
-            await this.cdaiToken.transfer(this.proxy1.address, amount("10"), { from: user1 })
-            expect(await this.cdaiToken.balanceOf(this.proxy1.address)).to.be.bignumber.equal(amount("10"))
+            await this.token.transfer(this.proxy1.address, amount("10"), { from: user1 })
+            expect(await this.token.balanceOf(this.proxy1.address)).to.be.bignumber.equal(amount("10"))
         })
 
         describe("with initial proxy token balance", function () {
             
             beforeEach(async function () {
-                await this.cdaiToken.transfer(await this.controller.proxiesByUser(user1), amount("10"), { from: user1 })
-                await this.cdaiToken.transfer(await this.controller.proxiesByUser(user2), amount("20"), { from: user2 })
+                await this.token.transfer(await this.controller.proxiesByUser(user1), amount("10"), { from: user1 })
+                await this.token.transfer(await this.controller.proxiesByUser(user2), amount("20"), { from: user2 })
             })
 
             it("doesn't let other users interact with the proxy", async function () {
-                expectRevert(this.proxy1.withdraw(this.cdaiToken.address, amount("10"), { from: user2 }), "Caller is not owner")
+                expectRevert(this.proxy1.withdraw(this.token.address, amount("10"), { from: user2 }), "Caller is not owner")
             })
 
             it("can't withdraw more tokens than proxy balance", async function () {
-                expectRevert(this.proxy1.withdraw(this.cdaiToken.address, amount("11"), { from: user1 }), "Insufficient funds")
+                expectRevert(this.proxy1.withdraw(this.token.address, amount("11"), { from: user1 }), "Insufficient funds")
             })
 
             it("can withdraw tokens from proxy", async function () {
-                const cdaiBalance = await this.cdaiToken.balanceOf(user1)
-                await this.proxy1.withdraw(this.cdaiToken.address, amount("10"), { from: user1 })
-                expect(await this.cdaiToken.balanceOf(user1)).to.be.bignumber.equal(cdaiBalance.add(amount("10")))
+                const cdaiBalance = await this.token.balanceOf(user1)
+                await this.proxy1.withdraw(this.token.address, amount("10"), { from: user1 })
+                expect(await this.token.balanceOf(user1)).to.be.bignumber.equal(cdaiBalance.add(amount("10")))
             })
 
         })
