3637c471e6d4c01b643f559461439eb6f71a3f51
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-08 03:50:52 +0100

    Add library for APWine naming generation

---
+++ contracts/libraries/APWineNaming.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.4.22 <0.7.3;
+
+library APWineNaming{
+    
+    function genTokenSymbol(uint8 _index,string memory _ibtSymbol, string memory _platfrom, string memory _periodDenominator) public pure returns (string memory){
+        return string(concatenate(string(concatenate(_periodDenominator,string(concatenate(uintToString(_index),_ibtSymbol)))),_platfrom));
+    }
+
+    function uintToString(uint v) public pure returns (string memory) {
+        bytes memory reversed = new bytes(8);
+        uint i = 0;
+        if(v==0) return "0";
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(uint8(48 + remainder));
+        }
+        bytes memory s = new bytes(i + 1);
+        for (uint j = 0; j <= i; j++) {
+            s[j] = reversed[i - j];
+        }
+        return string(s);
+    }
+    
+    function concatenate(
+        string memory a,
+        string memory b)
+        public
+        pure
+        returns(string memory) {
+            return string(abi.encodePacked(a, b));
+    }
+}
