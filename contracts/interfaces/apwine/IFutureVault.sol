584b4fcd1f17f8eadc667ca91097f073b573b6a2
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-20 12:29:07 +0100

    Update Future Vault interface

--- contracts/interfaces/apwine/IFutureVault.sol
+++ contracts/interfaces/apwine/IFutureVault.sol
@@ -6,8 +6,9 @@ interface IFutureVault {
     /**
     * @notice Intializer
     * @param _futureAddress the address of the corresponding future
+    * @param _adminAddress the address of the corresponding admin
     */  
-    function initialize(address _futureAddress) external;
+    function initialize(address _futureAddress,address _adminAddress) external;
 
 
     /**
@@ -15,5 +16,11 @@ interface IFutureVault {
     * @return the vinyard address linked to this future wallet
     */  
     function getFutureAddress() external view returns(address);
+
+
+    /**
+    * @notice Approve another token to be transfered from this contract by the future 
+    */  
+    function approveAdditionalToken(address _tokenAddress) external;
 
 }
