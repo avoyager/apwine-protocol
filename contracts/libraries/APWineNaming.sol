a0156b9f779bcaa5fb889c5d2af5656b937ba42f
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-20 14:54:18 +0100

    Update Naming library for token generation

--- contracts/libraries/APWineNaming.sol
+++ contracts/libraries/APWineNaming.sol
@@ -1,12 +1,34 @@
 pragma solidity >=0.7.0 <0.8.0;
 
+import '@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol';
+
+
 library APWineNaming{
-    
-    function genTokenSymbol(uint8 _index,string memory _ibtSymbol, string memory _platfrom, string memory _periodDenominator) public pure returns (string memory){
-        return string(concatenate(string(concatenate(_periodDenominator,string(concatenate(uintToString(_index),_ibtSymbol)))),_platfrom));
+    using SafeMathUpgradeable for uint256;
+
+
+    // i.e. 30D-AAVE-ADAI-2
+    function genFYTSymbol(uint8 _index,string memory _ibtSymbol, string memory _platfrom, uint256 _periodDuration) public pure returns (string memory){
+        return concatenate(genIBTSymbol(_ibtSymbol,_platfrom,_periodDuration),concatenate("-",uintToString(_index)));
     }
 
-    function uintToString(uint v) public pure returns (string memory) {
+    function genFYTSymbolFromIBT(uint8 _index, string memory _ibtSymbol) public pure returns (string memory){
+        return concatenate(_ibtSymbol,concatenate("-",uintToString(_index)));
+    }
+
+    function genIBTSymbol(string memory _ibtSymbol, string memory _platfrom, uint256 _periodDuration) public pure returns (string memory){
+        return concatenate(getPeriodDurationDenominator(_periodDuration),concatenate("-",concatenate(_platfrom,concatenate("-",_ibtSymbol))));
+    }
+
+    function getPeriodDurationDenominator(uint256 _periodDuration) public pure returns(string memory){
+        if (_periodDuration>=1 days){
+            uint256 numberOfdays = _periodDuration.div(1 days);
+            return string(concatenate(uintToString(uint8(numberOfdays)),"D"));
+        }
+        return "CUSTOM";
+    }
+
+    function uintToString(uint8 v) public pure returns (string memory) {
         bytes memory reversed = new bytes(8);
         uint i = 0;
         if(v==0) return "0";
