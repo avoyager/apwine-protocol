f5a1d19178b803c04af17340a9557e4a515a7b06
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-20 12:34:39 +0100

    Add GaugeController interface

---
+++ contracts/interfaces/apwine/IGaugeController.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+interface IController {
+    
+    enum FutureTypes {Weekly, Monthly}
+
+    function initialize(address _ADMIN, address _APW, address _registry) external;
+    function registerNewGauge(address _future) external returns(address);
+    function claimAPW(address _future) external ;
+    function mint(address _user, uint256 _amount) external;
+
+    
+    /* Getters */
+    function getLastEpochInflationRate() external view returns(uint256);
+
+    function getGaugeWeight() external view returns(uint256);
+
+    function getGaugeTypeWeight() external view returns(uint256);
+
+    function getEpochLength() external view returns(uint256);
+
+
+}
