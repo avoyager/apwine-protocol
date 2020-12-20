1a5e5467c3025fa022fdccf4dd120b0b965320fc
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-20 12:36:16 +0100

    Add FuturePlatformDeployer interface

---
+++ contracts/interfaces/apwine/IFuturePlatformDeployer.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+
+interface IFuturePlatformDeployer{
+
+    function initialize(address _controller, address _admin) external;
+
+    function deployFutureWithIBT(string memory _futurePlatformName,address _ibt, uint256 _periodDuration) external returns(address);
+
+}
