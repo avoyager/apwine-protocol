a73ac3bf0708443659a12b30adab375b559986c8
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2020-12-20 14:54:48 +0100

    Update controller with new future fonctionning

--- contracts/protocol/Controller.sol
+++ contracts/protocol/Controller.sol
@@ -1,39 +1,60 @@
 pragma solidity >=0.7.0 <0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/utils/EnumerableSetUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/EnumerableMapUpgradeable.sol";
+
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "contracts/interfaces/apwine/tokens/IFutureYieldToken.sol";
 import "contracts/interfaces/apwine/IFuture.sol";
+import "contracts/protocol/Registry.sol";
 
+import "contracts/libraries/APWineNaming.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 
 contract Controller is Initializable, AccessControlUpgradeable{
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.UintSet;
+    using SafeMathUpgradeable for uint256;
+
 
     /* ACR Roles*/
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     /* Attributes */
 
-    address public APWineTreasury;
-    address public APWineProxyFactory;
-    address public APWineIBTLogic;
-    address public FutureYieldTokenLogic;
+    Registry public registry;
+    mapping(uint256=>uint256) private nextPeriodSwitchByDuration; 
 
+    mapping(string=>EnumerableSetUpgradeable.UintSet) private platformNames;
 
-    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
-
-    EnumerableSetUpgradeable.AddressSet private futures;
+    EnumerableSetUpgradeable.UintSet private durations;
+    mapping(uint256=>EnumerableSetUpgradeable.AddressSet) private futuresByDuration; 
+    mapping(uint256=>uint256) private periodIndexByDurations;
 
     /* Events */
 
-    event FutureRegistered(address _futureAddress);
-    event FutureUnregistered(address _futureAddress);
-    event TreasuryAddressChanged(address _treasuryAddress);
+    event PlatformRegistered(address _platformControllerAddress);
+    event PlatformUnregistered(address _platformControllerAddress);
+    event NextPeriodSwitchSet(uint256 _periodDuration, uint256 _nextSwitchTimestamp);
+    event FutureRegistered(address _newFutureAddress);
+    event FutureUnregistered(address _future);
 
 
-    /* Future Settings */
+    /* PlatformController Settings */
     uint256 public STARTING_DELAY;
+    
 
     /* Modifiers */
+
+    modifier futureIsValid(address _future){
+        require(registry.isRegisteredFuture(_future),"incorrect future address");
+        _;
+    }
+
+    modifier futurePlatformDeployerIsValid(address _futurePlatformDeployerAddress){
+        require(registry.isRegisteredFuturePlatformDeployer(_futurePlatformDeployerAddress),"incorrect futurePlatform address");
+        _;
+    }
 
     /* Initializer */
 
@@ -46,70 +67,6 @@ contract Controller is Initializable, AccessControlUpgradeable{
         _setupRole(ADMIN_ROLE, _adminAddress);
     }
 
-    /* Public methods */
-
-    /**
-     * @notice Adds a future for everyone to use
-     * @param _futureAddress the address of the future
-     */
-    function addFuture(address _futureAddress) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        require(futures.add(_futureAddress), "Future already registered");
-        emit FutureRegistered(_futureAddress);
-    }
-
-    /**
-     * @notice Removes a future from the registered futures list
-     * @param _futureAddress the address of the future
-     */
-    function delFuture(address _futureAddress) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        require(futures.remove(_futureAddress), "Future not registered");
-        emit FutureUnregistered(_futureAddress);
-    }
-
-
-    /* Admin methods */
-
-    /**
-     * @notice Change the APWine treasury contract address
-     * @param _APWineTreasury the address of the new treasury contract
-     */
-    function setTreasuryAddress(address _APWineTreasury) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        APWineTreasury = _APWineTreasury;
-        emit TreasuryAddressChanged(_APWineTreasury);
-    }
-
-    /**
-     * @notice Change the APWineProxyFactory contract address
-     * @param _APWineProxyFactory the address of the new APWineProxyFactory contract
-     */
-    function setAPWineProxyFactoryAddress(address _APWineProxyFactory) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        APWineProxyFactory = _APWineProxyFactory;
-    }
-
-    /**
-     * @notice Change the APWineProxy contract logic address
-     * @param _FutureYieldTokenLogic the address of the new proxy logic
-     */
-    function setFutureYieldTokenLogic(address _FutureYieldTokenLogic) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        FutureYieldTokenLogic = _FutureYieldTokenLogic;
-    }
-
-    /**
-     * @notice Change the APWineIBT contract logic address
-     * @param _APWineIBTLogic the address of the new APWineIBTlogic
-     */
-    function setAPWineIBTLogic(address _APWineIBTLogic) public {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        APWineIBTLogic = _APWineIBTLogic;
-    }
-
-    /* Future Settings Setters */
-
     /**
      * @notice Change the delay for starting a new period
      * @param _startingDelay the new delay (+-) to start the next period
@@ -119,94 +76,126 @@ contract Controller is Initializable, AccessControlUpgradeable{
         STARTING_DELAY = _startingDelay;
     }
 
+    function setNextPeriodSwitchTimestamp(uint256 _periodDuration, uint256 _nextPeriodTimestamp) public {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not allowed to set next period timestamp");
+        nextPeriodSwitchByDuration[_periodDuration]=_nextPeriodTimestamp;
+        emit NextPeriodSwitchSet(_periodDuration,_nextPeriodTimestamp);
+    }
 
     /* User Methods */
 
-    /**
-     * @notice Register the sender to the corresponding future
-     * @param _futureAddress the address of the future to be registered to
-     * @param _amount the amount to register
-     */
-    function register(address _futureAddress, uint256 _amount) public {
-        require(futures.contains(_futureAddress), "Invalid future address");
-        IFuture future = IFuture(_futureAddress);
-        IFuture(_futureAddress).register(msg.sender,_amount);
-        require(ERC20(future.getIBTAddress()).transferFrom(msg.sender, address(_futureAddress),_amount), "Insufficient funds");
+    function register(address _future, uint256 _amount) public futureIsValid(_future){
+        IFuture(_future).register(msg.sender,_amount);
+    }
+
+    function unregister(address _future,uint256 _amount) public futureIsValid(_future){
+        IFuture(_future).unregister(msg.sender,_amount);
+    }
+
+    function withdrawLockFunds(address _future, uint256 _amount) public futureIsValid(_future){
+        IFuture(_future).withdrawLockFunds(msg.sender,_amount);
+    }
+
+    function claimFYT(address _future) public futureIsValid(_future){
+        IFuture(_future).claimFYT(msg.sender);
     }
 
     /**
-     * @notice Register the sender to the corresponding future
+     * @notice Register the sender to the corresponding platformController
      * @param _user the address of the user
-     * @param _futureAddress the addresses of the futures to claim the fyts from
+     * @param futuresAddresses the addresses of the futures to claim the fyts from
      */
-    function claimSelectedYield(address _user, address[] memory _futureAddress) public {
-        for(uint256 i = 0;  i<_futureAddress.length;i++){
-            require(futures.contains(_futureAddress[i]),"Incorrect future address");
-            IFuture(_futureAddress[i]).claimFYT(_user);
+    function claimSelectedYield(address _user, address[] memory futuresAddresses) public {
+        for(uint256 i = 0;  i<futuresAddresses.length;i++){
+            require(registry.isRegisteredFuture(futuresAddresses[i]),"Incorrect future address");
+            IFuture(futuresAddresses[i]).claimFYT(_user);
         }
     }
 
-    /* Views */
-
-    /**
-     * @notice Checks whether the address is a valid future
-     * @return bool true if the given future is valid
-     */
-    function isRegisteredFuture(address _futureAddress) public view returns (bool) {
-       return futures.contains(_futureAddress);
-    }
-
-    /**
-     * @notice Checks whether the address is a valid future
-     * @param _user the address of the user
-     * @return array of futures addresses where the user can claim fyt
-     * @dev shouldn't be called in a contract
-     */
-    function getFutureWithClaimableFYT(address _user) external view returns (address[] memory) {
-        address[] memory selectedFutures = new address[](futures.length());
+    /* User Getter */
+    function getFuturesWithClaimableFYT(address _user) external view returns (address[] memory) {
+        address[] memory selectedFutures = new address[](registry.futureCount());
         uint8 index = 0;
-        for (uint256 i = 0; i < futures.length(); i++) { 
-             if(IFuture(futures.at(i)).hasClaimableFYT(_user)){
-                 selectedFutures[i]= futures.at(i);
+        for (uint256 i = 0; i < registry.futureCount(); i++) { 
+             if(IFuture(registry.getFutureAt(i)).hasClaimableFYT(_user)){
+                 selectedFutures[i]= registry.getFutureAt(i);
                  index +=1;
              }
         }
         return selectedFutures;
     }
 
-    /**
-     * @notice Checks whether the address is a valid future
-     * @param _user the address of the user
-     * @return array of futures addresses where the user can claim ibt
-     * @dev shouldn't be called in a contract
-     */
-    function getFutureWithClaimableAPWIBT(address _user) external view returns (address[] memory) {
-        address[] memory selectedFutures = new address[](futures.length());
-        uint8 index = 0;
-        for (uint256 i = 0; i < futures.length(); i++) { 
-             if(IFuture(futures.at(i)).hasClaimableAPWIBT(_user)){
-                 selectedFutures[i]= futures.at(i);
-                index +=1;
-             }
+    /* Getter */
+
+    function getRegistery() external view returns(address){
+        return address(registry);
+    }
+
+    function getFutureIBTSymbol(string memory _ibtSymbol, string memory _platfrom, uint256 _periodDuration) public pure returns(string memory){
+        return APWineNaming.genIBTSymbol(_ibtSymbol, _platfrom,_periodDuration);
+    }
+
+    function getFYTSymbol(string memory _apwibtSymbol, uint256 _periodDuration) public view returns(string memory){
+        return APWineNaming.genFYTSymbolFromIBT(uint8(periodIndexByDurations[_periodDuration]), _apwibtSymbol);
+    }
+
+    function getPeriodIndex(uint256 _periodDuration) public view returns(uint256){
+        return periodIndexByDurations[_periodDuration];
+    }
+
+
+    function getNextPeriodStart(uint256 _periodDuration) public view returns(uint256){
+        return nextPeriodSwitchByDuration[_periodDuration];
+    }
+
+
+    /* future admin function*/
+    function registerNewFuture(address _newFuture) futurePlatformDeployerIsValid(msg.sender) public{
+        registry.addFuture(_newFuture);
+        uint256 futureDuration = IFuture(_newFuture).PERIOD();
+        if(!durations.contains(futureDuration)) durations.add(futureDuration);
+        registry.addFuture(_newFuture);
+        futuresByDuration[futureDuration].add(_newFuture);
+        emit FutureRegistered(_newFuture);
+    }
+
+    function unregisterFuture(address _future) public{
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        require(registry.removeFuture(_future),"invalid future");
+
+        uint256 futureDuration = IFuture(_future).PERIOD();
+        if(!durations.contains(futureDuration)) durations.remove(futureDuration);
+        futuresByDuration[futureDuration].remove(_future);
+        emit FutureUnregistered(_future);
+    }
+
+    function startFuturesByPeriodDuration(uint256 _periodDuration) public {
+        for(uint256 i = 0; i< futuresByDuration[_periodDuration].length() ;i++){
+            IFuture(registry.getFutureAt(i)).startNewPeriod();
         }
-        return selectedFutures;
+        nextPeriodSwitchByDuration[_periodDuration] = nextPeriodSwitchByDuration[_periodDuration].add(_periodDuration);
+        periodIndexByDurations[_periodDuration] = periodIndexByDurations[_periodDuration].add(1);
     }
 
-    /**
-     * @notice Number of future
-     * @return uint256 the number of future
-     */
-    function futureCount() external view returns (uint256) {
-        return futures.length();
+    function getFuturesWithDuration(uint256 _periodDuration) public view returns(address[] memory) {
+        address[] memory filteredFutures = new address[](futuresByDuration[_periodDuration].length());
+        for (uint256 i = 0; i < filteredFutures.length; i++) { 
+                 filteredFutures[i] = futuresByDuration[_periodDuration].at(i);
+        }
+        return filteredFutures;
     }
 
-    /**
-     * @notice View available future
-     * @param _index index of the future to retrieve
-     * @return address the future address at index
-     */
-    function future(uint256 _index) external view returns (address) {
-        return futures.at(_index);
+    /* Security functions */
+    function pauseFuture(address _future) public{
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        IFuture(_future).pausePeriods();
     }
+
+    function resumeFuture(address _future) public{
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        IFuture(_future).resumePeriods();
+    }
+
+
 
 }
