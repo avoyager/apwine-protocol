19e18a95e064054a378a0a8024d30bedfdab1479
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2021-01-16 03:59:18 +0100

    Update gaugeController interface

--- contracts/interfaces/apwine/IGaugeController.sol
+++ contracts/interfaces/apwine/IGaugeController.sol
@@ -9,24 +9,69 @@ interface IGaugeController {
         address _registry
     ) external;
 
+    /**
+     * @notice Deploy a new liquidity gauge for a newly created future and register in in the registry
+     * @param _future the address of the new future
+     * @return the address of the new liquidity gauge
+     */
     function registerNewGauge(address _future) external returns (address);
 
+    /**
+     * @notice Claim all claimable APW rewards for the sender
+     * @dev not gas efficient, claim function with specified liquidity gauges saves gas
+     */
     function claimAPW() external;
 
+    /**
+     * @notice Claim all claimable APW rewards for the sender for a specified list of liquidity gauges
+     * @param _liquidityGauges the the list of liquidity gauges to claim the rewards of
+     */
     function claimAPW(address[] memory _liquidityGauges) external;
 
-    function mint(address _user, uint256 _amount) external;
-
-    function addUserRedeemable(address _user, uint256 _amount) external;
+    /**
+     * @notice Admin function to pause APW whitdrawals
+     */
+    function pauseAPWWithdraw() external;
+    
+    /**
+     * @notice Admin function to resume APW whitdrawals
+     */
+    function resumeAPWWithdraw() external;
 
     /* Getters */
+
+    /**
+     * @notice Getter for the inflation rate of the current epoch
+     * @return the inflation rate of the current epoch
+     */
     function getLastEpochInflationRate() external view returns (uint256);
 
-    function getGaugeWeight() external view returns (uint256);
+    /**
+     * @notice Getter for weight of one gauge
+     * @param _liquidityGauge the address of the liquidity gauge to get the weight of
+     * @return the weight of the gauge
+     */
+    function getGaugeWeight(address _liquidityGauge) external view returns (uint256);
 
-    function getGaugeTypeWeight() external view returns (uint256);
-
+    /**
+     * @notice Getter for duration of one epoch
+     * @return the duration of one epoch
+     */
     function getEpochLength() external view returns (uint256);
 
+
+   /**
+     * @notice Getter for the total redeemable APW of one user
+     * @param _user the address of the user to get the redeemable APW of
+     * @return the total amount of APW redeemable
+     */
     function getUserRedeemableAPW(address _user) external view returns (uint256);
+
+
+   /**
+     * @notice Getter for the current state of rewards withdrawal availability
+     * @return true if the users can withdraw their redeemable APW, false otherwise
+     */
+    function getWithdrawableState() external view returns(bool);
+
 }
