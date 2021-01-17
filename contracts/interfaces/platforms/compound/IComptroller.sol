c1544d73d3ee00749f58028d8b787cbb2212e1bd
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2021-01-17 23:16:51 +0100

    Remove contracts for not-to-be-audited strategies

--- contracts/interfaces/platforms/compound/IComptroller.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.7.0 <0.8.0;
-
-interface Comptroller {
-    /**
-     * @notice Claim all the comp accrued by holder in all markets
-     * @param _holder The address to claim COMP for
-     */
-    function claimComp(address _holder) external;
-}
