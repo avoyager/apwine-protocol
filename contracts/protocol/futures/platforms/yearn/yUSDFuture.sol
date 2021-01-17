2b719aed5bc3d249c9517778a8a62cb28da03339
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2021-01-17 22:53:45 +0100

    Add yUSD Futures

---
+++ contracts/protocol/futures/platforms/yearn/yUSDFuture.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+import "contracts/protocol/futures/RateFuture.sol";
+import "contracts/interfaces/platforms/yearn/IyToken.sol";
+
+contract yUSDFuture is RateFuture {
+    function getIBTRate() public view override returns (uint256) {
+        return yToken(address(ibt)).getPricePerFullShare();
+    }
+}
---
+++ contracts/protocol/futures/platforms/yearn/yUSDFutureWallet.sol
@@ -0,0 +1,5 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+import "contracts/protocol/futures/futureWallets/RateFutureWallet.sol";
+
+contract yUSDFutureWallet is RateFutureWallet {}
