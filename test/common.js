46a1002a86f97fbd727671875d2caa64d4b5faaf
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2021-01-17 23:17:38 +0100

    Update commons with mainnet addresses for wanted pools

--- test/common.js
+++ test/common.js
@@ -1,6 +1,5 @@
 const { contract } = require("@openzeppelin/test-environment")
 
-const ADAI_ADDRESS = "0xfC1E690f61EFd961294b3e1Ce3313fBD8aa4f85d"
 
 module.exports = {
     contracts: {
@@ -28,8 +27,10 @@ module.exports = {
         APWineIBT: contract.fromArtifact('APWineIBT'),
     },
     ADDRESS_0: "0x0000000000000000000000000000000000000000",
-    WETH_ADDRESS: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    ADAI_ADDRESS,
+    AWETH_ADDRESS: "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e",
+    ADAI_ADDRESS:"0x028171bca77440897b824ca71d1c56cac55b68a3",
+    YUSD_ADDRESS: "0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c",
+    YDAI_ADDRESS:"0xC2cB1040220768554cf699b0d863A3cd4324ce32",
     FUTURE_DEPLOYER_ROLE: "0xdacd85ccbf3b93dd485a10886cc255d4fba1805ebed1521d0c405d4416eca3be",
     adai: contract.fromArtifact("ERC20", ADAI_ADDRESS),
     uniswapRouter: contract.fromABI(require("@uniswap/v2-periphery/build/IUniswapV2Router02.json").abi, undefined, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
