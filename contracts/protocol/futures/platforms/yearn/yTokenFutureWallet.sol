b8ddd80d9faad73de8da90bb14d220a2d12f5430
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2021-01-18 01:09:58 +0100

    Refactor yearn future

--- contracts/protocol/futures/platforms/yearn/yTokenFutureWallet.sol
+++ contracts/protocol/futures/platforms/yearn/yTokenFutureWallet.sol
@@ -2,4 +2,4 @@ pragma solidity >=0.7.0 <0.8.0;
 
 import "contracts/protocol/futures/futureWallets/RateFutureWallet.sol";
 
-contract yUSDFutureWallet is RateFutureWallet {}
+contract yTokenFutureWallet is RateFutureWallet {}
